aebe3ca927bb492c161c1f3c2c9d44d68225e867
murilofsouzaa <onemurilo@gmail.com>
2026-04-20 21:00:44 -0300

    qo03: second part from third exercise

--- chapter-1/test/q03/ReversingLetters.java
+++ chapter-1/test/q03/ReversingLetters.java
@@ -11,19 +11,43 @@ public class ReversingLetters {
         System.out.print("Type a phrase: ");
         String string = sc.nextLine();
 
+        char[] reversedLetters = string.toCharArray();
         
+        int start = 0;
+        for (int i = 0; i <= reversedLetters.length; i++) {
+            if(i == reversedLetters.length || reversedLetters[i] == ' '){
+                int end = i - 1;
+                while(start < end){
+                    char temp = reversedLetters[start];
+                    reversedLetters[start] = reversedLetters[end];
+                    reversedLetters[end] = temp;
+                    start++;
+                    end--;
+                }
+                start = i + 1;
+            }
+        }
+
         int k = 0;
         char[] reversedWordAndLetters = new char[string.length()];
-        for (int w = string.length() - 1; w >= 0; w--) {
-            reversedWordAndLetters[k] = string.charAt(w);
+        for (int i = string.length() - 1; i >= 0; i--) {
+            reversedWordAndLetters[k] = string.charAt(i);
             k++;
         }
 
-        System.out.println("\n");
 
+        System.out.println("\n");
+        
+        System.out.println("Letters:");
+        for (int i = 0; i < reversedLetters.length; i++) {
+            System.out.print(reversedLetters[i]);
+        }
+        
+        System.out.println("\n");
+        
         System.out.println("Words and letters:");
-        for (int w = 0; w < reversedWordAndLetters.length; w++) {
-            System.out.print(reversedWordAndLetters[w]);
+        for (int i = 0; i < reversedWordAndLetters.length; i++) {
+            System.out.print(reversedWordAndLetters[i]);
         }
 
         System.out.println("\n");
