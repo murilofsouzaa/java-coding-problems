f12c480e5037149fdd7bc8ec3351d5a6a27d205a
murilofsouzaa <onemurilo@gmail.com>
2026-04-21 23:58:33 -0300

    q04: finishing the fourth exercise

--- chapter-1/test/q04/ContaintOnlyDigits.java
+++ chapter-1/test/q04/ContaintOnlyDigits.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class ContaintOnlyDigits {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type a string: ");
+        String string = sc.nextLine();
+        boolean containOnlyDigits = true;
+        
+        
+        for (int i = 0; i < string.length(); i++) {
+            char character = string.charAt(i);
+            if(!(character >= '0' && character <= '9')){
+                containOnlyDigits = false;
+                break;
+            }
+        }
+        
+        System.out.println("Contains only digits: " + containOnlyDigits);
+    }
 }
