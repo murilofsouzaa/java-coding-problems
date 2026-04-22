a531f0699c3dfd1a81f5158c3af08b4a2a5bf09a
murilofsouzaa <onemurilo@gmail.com>
2026-04-22 19:53:05 -0300

    q05: finishing this question with a simples solution

--- chapter-1/test/q05/CountingVowelsCosonants.java
+++ chapter-1/test/q05/CountingVowelsCosonants.java
@@ -1,3 +1,31 @@
+//Write a program that counts the number of vowels and consonants in a given string. 
+//Do this for the English language, which has five vowels (a, e, i, o, and u).
+
+import java.util.Scanner;
 public class CountingVowelsCosonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+      
+        System.out.println("Type a string: ");
+        String string = sc.nextLine();
+
+        int countVowels = 0;
+        int countConsonants = 0;
+
+        char[] characters = string.toCharArray();    
+        for (int i = 0; i < characters.length; i++) {
+            if(characters[i] == 'a' || characters[i] == 'e' || characters[i] == 'i' || characters[i] == 'o' || characters[i] == 'u'){
+                countVowels++;
+            }else{
+                countConsonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + countVowels);
+        System.out.println("Consonants: " + countConsonants);
+
     
+
+    }
+
 }
